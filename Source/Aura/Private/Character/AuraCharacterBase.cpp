b93084af8e5876dcbd6d7fadc2f0c6dcfbf54008
yujin1025 <kyjc011006@gmail.com>
2025-04-11 20:50:48 +0900

    💻init : 기본 캐릭터 구현

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
